96d5b46f176334eaba5563cb2f1ce0b55d0a3d14
Alvaro Fernandez <kukillo1990@hotmail.com>
2016-11-05 01:03:59 +0000

    added first commit

---
+++ GameToSurvive/Source/GameToSurvive/GameToSurviveGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "GameToSurvive.h"
+#include "GameToSurviveGameMode.h"
+#include "GameToSurviveCharacter.h"
+
+AGameToSurviveGameMode::AGameToSurviveGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
